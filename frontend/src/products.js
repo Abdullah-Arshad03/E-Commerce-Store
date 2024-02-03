f77c13f21e020ef1419e41a881971c224d267e07
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-02-04 03:03:46 +0500

    working on the products frontend

---
+++ frontend/src/products.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+        _id : '1',
+        name : 'Airpods Wireless Bluetooth Headphone',
+        image : '/images/airpods.jpg',
+        description : 'Bluetooth techonologi lets you connect it with compatible devices wirelessly High-Quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand : 'Apple',
+        category : 'Electronis',
+        price : 89.99,
+        rating : 4.5 ,
+        numReviews : 12,
+    },
+    {
+        _id : '2',
+        name : 'Airpods Wireless Bluetooth Headphone',
+        image : '/images/airpods.jpg',
+        description : 'Bluetooth techonologi lets you connect it with compatible devices wirelessly High-Quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand : 'Apple',
+        category : 'Electronis',
+        price : 89.99,
+        rating : 4.5 ,
+        numReviews : 12,
+    },
+    {
+        _id : '3',
+        name : 'Airpods Wireless Bluetooth Headphone',
+        image : '/images/airpods.jpg',
+        description : 'Bluetooth techonologi lets you connect it with compatible devices wirelessly High-Quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        brand : 'Apple',
+        category : 'Electronis',
+        price : 89.99,
+        rating : 4.5 ,
+        numReviews : 12,
+    }
+]
+
+
+export {products}
---
+++ frontend/src/screens/homeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const homeScreen = () =>{
+    return(<>
+    
+    
+    </>)
+}
+
+export default homeScreen
