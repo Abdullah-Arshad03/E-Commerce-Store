fff578d601c067f95477b5c77b1d8d5e5b368491
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-02-09 03:43:18 +0500

    order model is created

---
+++ backend/models/orderModel.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        qty: {
+          type: Number,
+          required: true,
+        },
+        image: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        product: {
+          type: Schema.Types.ObjectId,
+          required: true,
+          ref: "Product",
+        },
+      },
+    ],
+    shippingAddress:{
+        address : {type : String , required : true},
+        city :{type : String , required:true},
+        postalCode : {type : Number, required : true},
+        country : {type : String , required :true}
+    },
+    paymentMethod : {
+        type : String ,
+        required : true
+    },
+    paymentResult : {
+        id : {type : String},
+        updateTime :{type:String},
+        status : {type:String},
+        emailAddress : {type:String}
+    },
+    itemsPrice :{
+        type : Number, 
+        required : true,
+        default : 0.0
+    },
+    taxPrice : {
+          type : Number ,
+          required : true,
+          default :0.0
+    },
+    shippingPrice : {
+        type : Number ,
+        required : true,
+        default : 0.0
+    },
+    totalPrice :{
+        type : Number, 
+        required : true,
+        default : 0.0 
+    },
+    isPaid : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+    paidAt : {
+        type : Date
+    },
+    isDelivered : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+    deliveredAt : {
+        type : Date
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const order = mongoose.model("Order", orderSchema);
+
+module.exports = order;
