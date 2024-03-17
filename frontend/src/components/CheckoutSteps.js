6fcff52645770f7c730d2c41d3c76c0d228cef2f
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-03-17 17:23:06 +0500

    checkout step component is created

---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Nav, NavItem , NavLink} from "react-bootstrap";
+import { LinkContainer} from "react-router-bootstrap";
+
+
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <>
+     <Nav className="justify-content-center mb-4">
+
+        <NavItem>
+
+        {step1 ? (<>
+        <LinkContainer to='/login'>
+            <NavLink className="text-dark"> <span style={{color: 'black', fontWeight: '500'}} > Sign in </span></NavLink>
+        </LinkContainer>
+       </>) : (<>
+       <NavLink  className="text-dark" aria-disabled>Sign in</NavLink>
+       </>) }
+       </NavItem>
+
+     
+
+       <NavItem>
+       {step2 ? (<>
+        <LinkContainer to='/shipping'>
+            <NavLink className="text-dark"> <span style={{color: 'black', fontWeight: '500'}} > Shipping </span></NavLink>
+        </LinkContainer>
+       </>) : (<>
+       <NavLink  className="text-dark" aria-disabled >Shipping</NavLink>
+       </>) }
+       </NavItem>
+
+       <NavItem>
+
+       {step3 ? (<>
+        <LinkContainer to='/payment'>
+            <NavLink className="text-dark"> <span style={{color: 'black', fontWeight: '500'}} > Payment </span></NavLink>
+        </LinkContainer>
+       </>) : (<>
+       <NavLink  className="text-dark" aria-disabled>Payment</NavLink>
+       </>) }
+       </NavItem>
+
+
+       <NavItem>
+       {step4 ? (<>
+        <LinkContainer to='/placeorder'>
+            <NavLink className="text-dark"> <span style={{color: 'black', fontWeight: '500'}} > Place Order </span></NavLink>
+        </LinkContainer>
+       </>) : (<>
+       <NavLink  className="text-dark" aria-disabled>Place Order</NavLink>
+       </>) }
+       </NavItem>
+
+       
+            </Nav>
+   </>
+  );
+};
+
+
+export default CheckoutSteps
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -6,6 +6,7 @@ import { Form, FormControl, FormGroup, FormLabel } from "react-bootstrap";
 import { Button } from "@mui/material";
 import { saveShippingAddress } from "../slices/cartSlice";
 import { useNavigate } from "react-router-dom";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = ()=>{
 
@@ -29,6 +30,7 @@ const ShippingScreen = ()=>{
 
     return(<>
   <FormContainer>
+    <CheckoutSteps step1={true} step2={true}></CheckoutSteps>
     <h1>Shipping</h1>
 
     <Form onSubmit={submitHandler}>
