cea4cca698adb6ff9eb1f7d68b87db5229a07d23
Abdullah Bin Arshad <aligatorabdullah@gmail.com>
2024-04-20 04:07:07 +0500

    added vercel url in the constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -7,4 +7,4 @@ export const AUTH_URL =  '/api/auth'
 export const ORDERS_URL =  '/api/orders';
 export const PAYPAL_URL =  '/api/config/paypal';
 export const UPLOAD_URL = '/api/upload'
-export const VERCEL_URL = 'https://e-commerce-store-fusf.vercel.app'
+export const VERCEL_URL = 'https://e-commerce-store-bay-xi.vercel.app'
